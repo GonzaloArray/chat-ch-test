8c2ffd080be82953a35d020f6336174edd318838
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-01-10 11:24:22 -0300

    simulation endpoint

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,21 @@
 import { RouterView } from 'vue-router';
 import NavBar from './components/header/NavBar.vue'
 
+
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  const response = await fetch('https://revhouse.azurewebsites.net/api/v1/token', {
+    headers: {
+      authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfdXNlcl9pZCI6IjkwODgzNWUxLTdmNDktNDQxZC05YjY5LTc1NGI1MThiODUxZiJ9.6hq6C4uxCrwNLS4G_rkcEzHWyzukfG1i3zwbNwR3lPI`,
+      'Content-Type': 'application/json'
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+});
+
+
 </script>
 
 <template>
